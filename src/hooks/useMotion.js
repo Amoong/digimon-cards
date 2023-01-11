fc0ac205d9c9c664d0f2f4d2090e5b35380e0b9e
Amoong <moo1323@naver.com>
2023-01-11 19:41:22 +0900

    Refactor useMotion

--- src/hooks/useMotion.js
+++ src/hooks/useMotion.js
@@ -2,7 +2,7 @@ import { useRef, useState } from "react";
 
 const isSetDone = (delta) => {
   const smallNum = 0.05;
-  return Math.abs(delta.rx) <= smallNum && Math.abs(delta.ry) <= smallNum;
+  return Object.values(delta).every((attr) => attr <= smallNum);
 };
 
 function useMotion(initialAttr) {
@@ -24,26 +24,20 @@ function useMotion(initialAttr) {
   const frame = (prev) => {
     const newAttr = newAttrRef.current;
 
-    const delta = {
-      rx: (newAttr.rx - prev.rx) / 20,
-      ry: (newAttr.ry - prev.ry) / 20,
-      gx: (newAttr.gx - prev.gx) / 20,
-      gy: (newAttr.gy - prev.gy) / 20,
-      go: (newAttr.go - prev.go) / 20,
-    };
+    const delta = Object.keys(newAttr).reduce((acc, key) => {
+      acc[key] = (newAttr[key] - prev[key]) / 20;
+      return acc;
+    }, {});
 
     if (isSetDone(delta)) {
       curFrameIdRef.current = null;
       return;
     }
 
-    const next = {
-      rx: prev.rx + delta.rx,
-      ry: prev.ry + delta.ry,
-      gx: prev.gx + delta.gx,
-      gy: prev.gy + delta.gy,
-      go: prev.go + delta.go,
-    };
+    const next = Object.keys(delta).reduce((acc, key) => {
+      acc[key] = delta[key] + prev[key];
+      return acc;
+    }, {});
 
     setAttr(next);
 
